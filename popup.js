44ab604fec7437c8f061b01e505f2f9c89ab31a2
Louis Maddox <naivelocus@gmail.com>
2014-05-21 13:49:46 +0100

    New icons

--- popup.js
+++ popup.js
@@ -1,23 +1,21 @@
 function tabDL(){
-tabUrls = [];
+    tabUrls = [];
 chrome.tabs.query({currentWindow: true, active: false}, function(tabs){
     var div = document.createElement('div');
 for (i=0;i<tabs.length;i++) {
-	tabUrls.push(tabs[i].url);
+    tabUrls.push(tabs[i].url);
 }
-	header = document.createElement('h1');
-	header.innerHTML = 'Downloads in progress';
-	header.setAttribute('style','line-height:0.6; font-size: 1.6em;');
-	div.innerHTML = header.outerHTML + tabUrls.join('<br>');
-	div.setAttribute('style','width: 600px; font-family: Helvetica, Tahoma; font-size: smaller; margin: 1.5em; cursor: default; line-height: 2; border: 3px solid #39A799; padding: 1em;');
-	document.body.appendChild(div);
-
+    header = document.createElement('h1');
+    header.innerHTML = 'Downloads in progress';
+    header.setAttribute('style','line-height:0.6; font-size: 1.6em;');
+    div.innerHTML = header.outerHTML + tabUrls.join('<br>');
+    div.setAttribute('style','width: 600px; font-family: Helvetica, Tahoma; font-size: smaller; margin: 1.5em; cursor: default; line-height: 2; border: 3px solid #39A799; padding: 1em;');
+    document.body.appendChild(div);
 for (i=0;i<tabUrls.length;i++) {
-	chrome.downloads.download({url: tabUrls[i]});
+    chrome.downloads.download({url: tabUrls[i]});
 }
 });
-}; 
-
+};     
 document.addEventListener('DOMContentLoaded', function () {
-  tabDL();
+     tabDL();
 });
