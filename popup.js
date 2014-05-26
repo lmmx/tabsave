df1371cb5d905a1fda5ad7ecc13f3b4855ca1444
Louis <naivelocus@gmail.com>
2014-05-26 08:01:43 +0100

    New version with manual list - currently without same deletion buttons as tab list (which don't work anyway)

--- popup.js
+++ popup.js
@@ -2,9 +2,9 @@ function stylePop() {
 	inStyle = document.createElement('style');
 	inStyle.type = 'text/css';
 	inStyle.id = 'instyle';
-	inStyle.innerHTML = '#main {width: 600px; font-family: "SEGOEUIL", Helvetica, Tahoma; font-size: smaller; margin: 1.5em 1.5em 0.5em 1.5em; cursor: default; line-height: 2; border: 3px solid #39A799; padding: 1em;} #main h1 { font-weight: normal; line-height:1; font-size: 50px; margin: 0 0 0.4em 0; } input[type="url"] { border:none; outline: none; width: 80%; } #tablist {overflow-y: scroll !important; max-height: 400px; } #tablist p { width: 97%;overflow-y: overlay;white-space: nowrap; text-overflow: ellipsis; margin:0; padding-bottom: 2px; } #tablist p:active, #tablist p:focus {text-overflow: initial; outline: none; } #p p::selection {background: #62C9BC;} div#p{overflow:auto!important; width: 100% }  a.close {-webkit-box-shadow: inset 0 0 5px 0 rgba(255,255,255,0.5);line-height: 1.1;font-size: 15px;display: block;height: 18px;-webkit-border-radius: 32px;float: right;padding: 0 4px;margin-top: -24px;transition: all 0.4s ease-out;} #p p:active + .close, #p p:focus + .close {opacity: 0;} #p p:hover + .close, .close:hover {font-weight: bold;color: white;background-color: #CFCFCF; } /*Scrollbars*/  p::-webkit-scrollbar { display: none; } #textin::-webkit-scrollbar, #tablist::-webkit-scrollbar {width: 18px;} #textin::-webkit-scrollbar-track, #tablist::-webkit-scrollbar-track {background-color: transparent;} #textin::-webkit-scrollbar-thumb, #tablist::-webkit-scrollbar-thumb {border: 4px solid rgba(0, 0, 0, 0);-webkit-border-radius:16px;background-clip: padding-box;-webkit-box-shadow: inset 0 0 0px rgba(0,0,0,.3);background-color: #dadada;} #txtin::-webkit-scrollbar-corner, #tablist::-webkit-scrollbar-corner {background-color: transparent;} /* Buttons */ section {margin: 0 1.5em; height:24px; border: 3px solid transparent;}section > svg {float: left;} svg * {fill: #315C5E;} #editbtn { background: none; border: 0; outline: none; height: 27px; } #btnConvert {float: right; background: #3BA294;border: 0;border-radius: 430px;color: white;font-family: "SEGOEUIL";font-size: 1em;outline:none;box-shadow: inset 0 0 5px 0 rgba(255,255,255,0.5);-webkit-box-shadow: inset 4px 4px 6px 0 rgba(255,255,255,0.5);padding: 5px 7px 0 7px;line-height: 1;text-shadow: white 0px 0px 20px, white 0px 0px 20px, white 0px 0px 20px;} #btnConvert::before {content: "Download";font-size: smaller;vertical-align: super;padding-right: 5px;} #btnConvert > svg * {fill: #FFF;} #btnConvert:active, #btnConvert:visited {margin: 2px 2px 0 0;border: 1px solid rgba(153, 153, 153, 0.59); /*{Shadow}*/ box-shadow:inset 0 0 5px 0 #333; -moz-box-shadow:inset 0 0 5px 0 #333; -webkit-box-shadow:inset 0 1px 2px 0 #333, 0 1px 1px 0 #fff;text-shadow: none;} /* Editing */ #main > textarea {outline:none; width: 90%; padding:8px; font-family: "SEGOEUIL"; border: 0; border-style: groove;background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(251, 247, 255, 0.63)), color-stop(100%,rgba(229, 241, 255, 0.51)));height: 146px; margin: 0px;}'
+	inStyle.innerHTML = '* {font-family: "SEGOEUIL", Helvetica, Tahoma, sans-serif;} #main {width: 600px;font-size: smaller;margin: 1.5em 1.5em 0.5em 1.5em;cursor: default;line-height: 2;border: 6px solid rgba(57, 167, 153, 0.61);border-radius: 0 36px;padding: 1.5em;} #main h1 { font-weight: normal; line-height:1; font-size: 50px; margin: 0 0 0.4em 0; } #main > * { margin-left: 20px !important; } input[type="url"] { border:none; outline: none; width: 80%; } #tablist {overflow-y: scroll !important; max-height: 400px; } #tablist p { width: 97%;overflow-y: overlay;white-space: nowrap; text-overflow: ellipsis; margin:0; padding-bottom: 2px; } #tablist p:active, #tablist p:focus {text-overflow: initial; outline: none; } #p p::selection {background: #62C9BC;} div#p{overflow:auto!important; width: 100% }  a.close {-webkit-box-shadow: inset 0 0 5px 0 rgba(255,255,255,0.5);line-height: 1.1;font-size: 15px;display: block;height: 18px;-webkit-border-radius: 32px;float: right;padding: 0 4px;margin-top: -24px;-webkit-transition: all 0.4s ease-out; transition: all 0.4s ease-out;} #p p:active + .close, #p p:focus + .close {opacity: 0;} #p p:hover + .close, .close:hover {font-weight: bold;color: white;background-color: #CFCFCF; } /*Scrollbars*/  p::-webkit-scrollbar { display: none; } #textin::-webkit-scrollbar, #tablist::-webkit-scrollbar {width: 18px;} #textin::-webkit-scrollbar-track, #tablist::-webkit-scrollbar-track {background-color: transparent;} #textin::-webkit-scrollbar-thumb, #tablist::-webkit-scrollbar-thumb {border: 4px solid rgba(0, 0, 0, 0);-webkit-border-radius:16px;background-clip: padding-box;-webkit-box-shadow: inset 0 0 0px rgba(0,0,0,.3);background-color: #dadada;} #txtin::-webkit-scrollbar-corner, #tablist::-webkit-scrollbar-corner {background-color: transparent;} /* Buttons */ section {margin: 0 1.5em; height:24px; border: 3px solid transparent;}section > svg {float: left;} svg * {fill: #315C5E;} #editbtn { background: none; border: 0; outline: none; height: 27px; } #btnDL {float: right; background: rgba(57, 167, 153, 0.91);margin-top:-4px; border: 0;border-radius: 430px;color: white;font-size: 1em;outline:none;box-shadow: inset 0 0 5px 0 rgba(255,255,255,0.5);-webkit-box-shadow: inset 4px 4px 6px 0 rgba(255,255,255,0.5);padding: 5px 7px 0 7px;line-height: 1.2;text-shadow: white 0px 0px 20px, white 0px 0px 20px; -webkit-transition: all 0.4s ease-out; transition: all 0.4s ease-out;} #btnDL > svg * {fill: #FFF;} #btnDL:active, #btnDL:visited {margin-top: -4px;border: 1px solid rgba(153, 153, 153, 0.59); /*{Shadow}*/ box-shadow:inset 0 0 5px 0 #333; -moz-box-shadow:inset 0 0 5px 0 #333; -webkit-box-shadow:inset 0 1px 2px 0 #333, 0 1px 1px 0 #fff;text-shadow: none;} /* Editing */ #txtin {outline:none; border-radius:16px; width: 90%; padding:8px; border: 0; border-style: groove;background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(251, 247, 255, 0.63)), color-stop(100%,rgba(229, 241, 255, 0.51)));height: 146px; margin: 0px; -webkit-transition: all 0.4s ease-out; transition: all 0.6s ease-out 0.4s; } input[type="url"] + #txtin { height: 80px; }'
 	if (navigator.appVersion.indexOf("Win")>-1) {
-    inStyle.innerHTML += " /* Font */ @font-face{ font-family: \"SEGOEUIL\"; src: local('☺'), url('https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.svg') format(\"svg\"), url('https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.woff') format('woff'), url('https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.ttf') format('truetype');}";
+    inStyle.innerHTML += " /* Font */ @font-face{ font-family: 'SEGOEUIL'; src: local('☺'), url('https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.svg') format(\"svg\"), url('https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.woff') format('woff'), url('https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.ttf') format('truetype');}";
 }
 	else inStyle.innerHTML += ' /* Font */ @font-face {font-family: "SEGOEUIL";src: url(\'https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.eot\');src: local("Segoe UI Light"), url(\'https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.woff\') format("woff"), url(\'https://raw.githubusercontent.com/lmmx/websiteresources/master/fonts/SEGOEUIL.ttf\') format("truetype");}'
 	document.body.appendChild(inStyle);
@@ -17,6 +17,38 @@ function isURL(str) { // URL checking RegEx courtesy of Matthew O'Riordan http:/
   } else {
     return true;
   }
+}
+
+function pasteIn(e) {
+	txt = document.querySelector('#txtin');
+	setTimeout(function(){
+		temparray = []; tabUrls = [];
+		temparray.push(txt.value.replace(/,\s|\r|\f/g,'\n').replace(/\n{2,}/g,'\n').split('\n'));
+		for (k=0;k<temparray[0].length;k++) {
+			tabUrls.push(temparray[0][k]);
+		}
+		URLn = tabUrls[tabUrls.length-1];
+    if (!isURL(URLn)) {
+      (URLn == '' || URLn == ' ' || URLn == '\t') ? txt.value = null :	txt.value = URLn;
+      tabUrls.pop([tabUrls.length]);
+	}
+	else txt.value = null;
+	urlHolder = document.createElement('div');
+	document.body.appendChild(urlHolder);
+	for (i=0;i<tabUrls.length;i++) {
+		urlbox = document.createElement('input');
+		urlbox.type = 'url';
+		urlbox.value = tabUrls[i];
+		document.querySelector('#main').insertBefore(urlbox,document.querySelector('textarea'));
+	}
+	}, 0);
+}
+
+function enterUp(e) {
+	if (e.keyCode === 13) {
+		console.log("Logging this one");
+		pasteIn();
+	}
 }
 
 function urlGet(){
@@ -44,11 +76,11 @@ for (i=0;i<tabs.length;i++) {
 	dlSVG = '<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="12px" height="18px" viewBox="0 0 23 35" preserveAspectRatio="xMidYMid meet"><g transform="scale(0.02)"><g transform="translate(0.000000,1750.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none"><path d="M3170 11440 l0 -5700 -1502 0 -1503 0 2798 -2797 2797 -2798 2797 2798 2798 2797 -1503 0 -1502 0 0 5700 0 5700 -2590 0 -2590 0 0 -5700z"></path></g></g></svg>'
   btn.innerHTML = dlSVG;
 	btn.id = 'dlbtn';
-  btn.setAttribute('id','btnConvert');
+  btn.setAttribute('id','btnDL');
   formsect.innerHTML = editbtn.outerHTML + btn.outerHTML;
   document.body.appendChild(formsect);
-	document.querySelector('#btnConvert').addEventListener('click', function(e) {
-		// tabDL();
+	document.querySelector('#btnDL').addEventListener('click', function(e) {
+		tabDL();
 	})
   document.querySelector('#editbtn').addEventListener('click', function(e) {
 	textarea = document.createElement('textarea');
@@ -58,50 +90,24 @@ for (i=0;i<tabs.length;i++) {
 	div.childNodes[0].outerHTML = edHead.outerHTML;
 	div.childNodes[1].outerHTML = textarea.outerHTML;
 
-	document.querySelector('textarea').addEventListener('onpaste', function () {
-		txt = document.querySelector('textarea');
-		txt.previousSibling.innerHTML = 'hello';
-		tabUrls = txt.value.replace(/,\s|\r|\f/g,'\n').replace(/\n{2,}/g,'\n').split('\n');
-		URLn = tabUrls[tabUrls.length-1];
-	    if (!isURL(URLn)) {
-	      (URLn == '' || URLn == ' ' || URLn == '\t') ? txt.value = null :	txt.value = URLn;
-	      tabUrls.pop([tabUrls.length]);
-		}
-		else txt.value = null;
-		urlHolder = document.createElement('div');
-		document.body.appendChild(urlHolder);
-		for (i=0;i<tabUrls.length;i++) {
-			urlbox = document.createElement('input');
-			urlbox.type = 'url';
-			urlbox.value = tabUrls[i];
-			document.querySelector('#main').insertBefore(urlbox,document.querySelector('textarea'));
-		}
-	});
+	document.querySelector('textarea').addEventListener('paste', pasteIn, false);
+	document.querySelector('textarea').addEventListener('keyup', enterUp, false);
 	this.remove();
 	document.querySelector('textarea').focus();
 	}, false);
-  alert(div.childNodes[1].innerText);
-	document.querySelector('#btnConvert').focus();
+	document.querySelector('#btnDL').focus();
 	
 });
 
 closers = document.querySelectorAll('.close');
-	for (i=0;i<closers.length;i++) {
+	for (c=0;c<closers.length;c++) {
 		closers[i].addEventListener('click',function(e) {
 			alert("hello?")
 			this.parentNode.parentNode.remove();
 			alert("hello!");
-		})
+		}, false)
 	}
 
-}
-
-function editList(){
-  document.querySelector('div:not(#main)').remove();
-  editIn = document.body.createElement('input');
-  editIn.setAttribute('placeholder','paste links here');
-  editIn.setAttribute('type','url');
-  document.body.appendChild(editIn);
 }
 
 function tabDL(){
